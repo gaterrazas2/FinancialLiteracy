e5fad3ba5b8d779df1e17a4bddc3d73720ac920a
Gabriel Terrazas <Gaboon@Gabes-Mac.local>
2023-10-13 20:56:44 -0600

    Updated calendar to include 2023

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -47,7 +47,7 @@ function ExpenseForm(props){
 
             <div className='new-expense__control'> 
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChanger} required/>
+                <input type="date" min="2019-01-01" max="2023-12-31" value={enteredDate} onChange={dateChanger} required/>
             </div>
         </div>
         <div className='new-expense__actions'>
