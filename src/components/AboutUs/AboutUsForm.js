a6d386f6a66f769247fa07c25b167b9dc5fc16b5
Gabriel Terrazas <Gaboon@Gabes-Mac.local>
2023-10-13 20:51:42 -0600

    Added about me paragraph for Gabriel Terrazas

--- src/components/AboutUs/AboutUsForm.js
+++ src/components/AboutUs/AboutUsForm.js
@@ -3,9 +3,10 @@ function AboutUsForm(props){
     return(
         <center className="aboutUs-form">
             <h1>Dream Team</h1>
-            <h2>Gabriel Terrazas</h2>
-            <h2>Emilia Rivas</h2>
-            <h2>Diego Acosta</h2>
+            <h3>Gabriel Terrazas</h3>
+            <div>Dedicated instructional assistant with a passion for empowering learners. A proud alumni ambassador for AI4ALL. Currently in my senior year, preparing to make a mark in the world of technology</div>
+            <h3>Emilia Rivas</h3>
+            <h3>Diego Acosta</h3>
             <button onClick={props.onCancel}>Close</button>
         </center>
     )
