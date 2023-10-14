30bbecac458ca8376f78aa100d7e14cee8324251
Gabriel Terrazas <Gaboon@Gabes-Mac.local>
2023-10-13 20:40:01 -0600

    Created compound interest formula to calculate loans

--- src/components/Calculator/LoanInfo.js
+++ src/components/Calculator/LoanInfo.js
@@ -1,14 +1,22 @@
+import Card from '../UI/Card'
+import './LoanInfo.css'
 function LoanInfo(props){
-
+    // compound interest formula
+    const amount = props.amount
+    const numYears = props.numYears
+    const interestRate = props.interestRate
+    const temp = amount * ((1 + interestRate) ** numYears)
+    const loanTotal = temp.toFixed(2)
 
     return(
-        <center>
-            <div>Loan Info</div>
-            <div>Amount: {props.amount}</div>
-            <div>Number of Years: {props.numYears}</div>
-            <div>Interest Rate: {props.interestRate}</div>
+        <Card className="loan">
+            <h2>Loan Info</h2>
+            <h3>Amount: {props.amount}</h3>
+            <h3>Number of Years: {props.numYears}</h3>
+            <h3>Interest Rate: {props.interestRate}</h3>
+            <h3>Loan Total: {loanTotal}</h3>
             <button onClick={props.onClose}>Close</button>
-        </center>
+        </Card>
     )
 }
 
