f32914382366719ac049884906501fbb5401a5ca
EmiliaR8 <rivasemilia2@gmail.com>
2023-10-17 22:28:11 -0600

    editted the calculator

--- src/components/Calculator/LoanInfo.js
+++ src/components/Calculator/LoanInfo.js
@@ -1,8 +1,12 @@
 function LoanInfo(props){
+    //calculate the payments per month, total amount paid, and total interest
+    const monthlyPay = props.amount*((props.interestRate*((1+props.interestRate)**12)) / (((1+props.interestRate)**12) -1))
+    // const interestPay = props.amount*(props.interestRate / )  //props.amount*(props.interestRate/12*props.numYears)
+
     return(
         <center>
             <div>Loan Info</div>
-            <div>Amount: {props.amount}</div>
+            <div>Monthly Payment: {monthlyPay}</div>
             <div>Number of Years: {props.numYears}</div>
             <div>Interest Rate: {props.interestRate}</div>
             <button onClick={props.onClose}>Close</button>
