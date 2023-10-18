e2bd68d06a7d7c6baa8b45600a0aaaba2c498c0f
Gabriel Terrazas <Gaboon@Gabes-Mac.local>
2023-10-18 09:32:48 -0600

    Diego About me

--- src/components/AboutUs/AboutUsForm.js
+++ src/components/AboutUs/AboutUsForm.js
@@ -8,6 +8,7 @@ function AboutUsForm(props){
             <h3>Emilia Rivas</h3>
             <div>I am a senior in computer science, and I have learned most about credit in this class</div>
             <h3>Diego Acosta</h3>
+            <div>A dedicated learner with a passion for computer science, currently a sophomore striving to become a future software engineer.</div>
             <button onClick={props.onCancel}>Close</button>
         </center>
     )
